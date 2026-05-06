434dab7c1666f43efb61216b6cea57a8e7c89374
Aayan Mahmood <mahmoodaayan79@gmail.com>
2026-05-06 03:20:17 +0100

    add neural network - Iris dataset 100% accuracy (correct)

--- neural_network.py
+++ neural_network.py
@@ -4,7 +4,6 @@ import torch.optim as optim
 from sklearn.datasets import load_iris
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
-import numpy as np
 
 # Loading the data
 iris = load_iris()
